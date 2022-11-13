e2839bb510af2a1b328f2834915f1e090e3c601c
tuyojr <olutuyod@gmail.com>
2022-11-13 20:32:54 +0100

    calculate the Average Electricity Bill

---
+++ arithmeticOperators.py
@@ -0,0 +1,12 @@
+# It's time to try a calculation in Python!
+# My electricity bills for the last three months have been 
+# $23, $32 and $64. What is the average monthly electricity 
+# bill over the three month period? Write an expression to 
+# calculate the mean, and use print() to view the result.
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+
+print((23 + 32 + 64)/3)
+
+
+
