1d7c1e6be31697323b881957044d97aa5796f1e8
tuyojr <olutuyod@gmail.com>
2022-11-13 20:38:37 +0100

    simple arithmetic to find the number of left-over tiles

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -12,3 +12,17 @@ print((23 + 32 + 64)/3)
 
 
 
+# In this quiz you're going to do some calculations for a tiler. 
+# Two parts of a floor need tiling. One part is 9 tiles wide by 
+# 7 tiles long, the other is 5 tiles wide by 7 tiles long. 
+# Tiles come in packages of 6.
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each. How many 
+# tiles will be left over?
+
+# Fill this in with an expression that calculates how many tiles are needed.
+noOfTiles = (9*7)+(5*7)
+print(noOfTiles)
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((6*17) - noOfTiles)
