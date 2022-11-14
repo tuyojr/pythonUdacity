978f1a1de7554f647431b793d96d5ef4e3f52e50
tuyojr <olutuyod@gmail.com>
2022-11-14 16:15:47 +0100

    working with more complex dictionary

---
+++ compound_data_structures.py
@@ -0,0 +1,22 @@
+# Quiz: Adding Values to Nested Dictionaries
+# Try your hand at working with nested dictionaries. 
+# Add another entry, 'is_noble_gas,' to each dictionary 
+# in the elements dictionary. After inserting the new 
+# entries you should be able to perform these lookups:
+# >>> print(elements['hydrogen']['is_noble_gas'])
+# False
+# >>> print(elements['helium']['is_noble_gas'])
+# True
+
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+# todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
+# hint: helium is a noble gas, hydrogen isn't
+
+elements['hydrogen']['is_noble_gas'] = False
+elements['helium']['is_noble_gas'] = True
+
+print(elements['hydrogen']['is_noble_gas'])
+print(elements['helium']['is_noble_gas'])
+
