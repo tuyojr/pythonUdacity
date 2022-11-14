394cd24ae7695438c36f167491e035d51313aa10
tuyojr <olutuyod@gmail.com>
2022-11-14 12:47:14 +0100

    working with some list methods

---
+++ list_methods.py
@@ -0,0 +1,34 @@
+# Quiz: len, max, min, and Lists
+# There is a Python environment for you to run test code 
+# at the bottom of this page related to any of the quizzes 
+# on this page!
+
+# What would the output of the following code be? 
+# (Treat the comma in the multiple choice answers as newlines.)
+
+# a = [1, 5, 8]
+# b = [2, 6, 9, 10]
+# c = [100, 200]
+
+# print(max([len(a), len(b), len(c)])) ===> 4
+# print(min([len(a), len(b), len(c)])) ===> 2
+
+
+
+# Quiz: sorted, join, and Lists
+# What would the output of the following code be? 
+# (Treat the comma in the multiple choice answers as newlines.)
+
+# names = ["Carol", "Albert", "Ben", "Donna"]
+# print(" & ".join(sorted(names))) ===> Albert & Ben & Carol & Donna
+
+
+
+# Quiz: append and Lists
+
+# What would the output of the following code be? 
+# (Treat the commas in the multiple choice answers as newlines.)
+
+# names = ["Carol", "Albert", "Ben", "Donna"]
+# names.append("Eugenia")
+# print(sorted(names)) ===> ["Albert", "Ben", "Carol", "Donna", "Eugenia"]
