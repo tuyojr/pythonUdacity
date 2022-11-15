4e5744043766c9ba4ba2294fe0b4bffb8aeb75a4
tuyojr <olutuyod@gmail.com>
2022-11-15 12:07:52 +0100

    a simple loop to iterate over a list and print each word on a line

---
+++ for_loops.py
@@ -0,0 +1,20 @@
+# Practice: Quick Brown Fox
+# Use a for loop to take a list and print each element of the 
+# list in its own line.
+
+sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+# Output:
+# the
+# quick
+# brown
+# fox
+# jumped
+# over
+# the
+# lazy
+# dog
+
+for word in range(len(sentence)):
+    print(sentence[word])
+
