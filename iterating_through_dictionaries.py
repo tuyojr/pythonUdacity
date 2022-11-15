903e5a2ce90838b5555b958038dcc9785815f777
tuyojr <olutuyod@gmail.com>
2022-11-15 16:19:17 +0100

    iterating through a dicitionary to get the amount of available fruits and non-fruits

--- iterating_through_dictionaries.py
+++ iterating_through_dictionaries.py
@@ -64,3 +64,35 @@ print(result)
 
 
 
+
+
+# Quiz: Fruit Basket - Task 3
+# So, a couple of things about the above examples:
+
+# It is a bit annoying having to copy and paste all the code to each spot - 
+# wouldn't it be nice to have a way to repeat the process without copying 
+# all the code? Don't worry! You will learn how to do this in the next lesson!
+# It would be nice to keep track of both the number of fruits and other items 
+# in the basket.
+
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key, value in basket_items.items():
+#if the key is in the list of fruits, add to fruit_count.
+    if key in fruits:
+        fruit_count += value
+#if the key is not in the list, then add to the not_fruit_count
+    elif key not in fruits:
+        not_fruit_count += value
+
+print(fruit_count, not_fruit_count)
+
+
