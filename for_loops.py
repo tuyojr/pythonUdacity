51490b2d117089e0cba3046fdb9741d554cfb9f4
tuyojr <olutuyod@gmail.com>
2022-11-15 12:22:45 +0100

    simple for loop to iterate over a list of names and changing to lower snake case and appending to a new list

--- for_loops.py
+++ for_loops.py
@@ -39,3 +39,30 @@ for word in sentence:
 for multiple in range(5, 35, 5):
     print(multiple)
 
+
+
+
+
+# Quiz: Create Usernames
+# Write a for loop that iterates over the names list to create a usernames list. 
+# To create a username for each name, make everything lowercase and replace spaces 
+# with underscores. Running your for loop over the list:
+
+# names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+# should create the list:
+
+# usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+
+# HINT: Use the .replace() method to replace the spaces with underscores. 
+# Check out how to use this method here 
+# https://stackoverflow.com/questions/12723751/replacing-instances-of-a-character-in-a-string/12723785#12723785
+
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+
+# write your for loop here
+for name in names:
+    usernames.append(name.lower().replace(' ', '_'))
+
+print(usernames)
