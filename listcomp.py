5844922ad09ac0662c4c43f5379abcc6df79166d
tuyojr <olutuyod@gmail.com>
2022-11-17 15:37:46 +0100

    using the listcomp to filter names in a dictionary, by scores

--- listcomp.py
+++ listcomp.py
@@ -6,3 +6,21 @@ names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smi
 
 first_names = [name.split()[0].lower() for name in names] # write your list comprehension here
 print(first_names)
+
+
+
+
+# Quiz: Filter Names by Scores
+# Use a list comprehension to create a list of names passed that only include those that scored at least 65.
+
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65] # write your list comprehension here
+
+print(passed)
