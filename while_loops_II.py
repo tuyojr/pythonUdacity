428633a611f4c24e2d10535f105acbbf13df5a0e
tuyojr <olutuyod@gmail.com>
2022-11-17 06:14:53 +0100

    while loop to find the nearest square less than a limit

--- while_loops_II.py
+++ while_loops_II.py
@@ -58,3 +58,28 @@ else:
 
 
 print(result)
+
+
+
+
+# Quiz: Nearest Square
+# Write a while loop that finds the largest square number less than an 
+# integer limit and stores it in a variable nearest_square. A square 
+# number is the product of an integer multiplied by itself, for example 
+# 36 is a square number because it equals 6*6.
+
+# For example, if limit is 40, your code should set the nearest_square to 36.
+
+limit = 40
+
+# write your while loop here
+num = 0
+
+# first, while the squared value of num is less than the limit, increase the value of num
+# set the value of the nearest_sqaure less than the limit to the value of num squared 
+while (num+1)**2 < limit:
+    num += 1
+nearest_square = num**2
+
+
+print(nearest_square)
