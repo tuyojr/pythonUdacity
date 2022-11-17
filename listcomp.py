0209af5dd4822304b86e11a90553557e7570d7cc
tuyojr <olutuyod@gmail.com>
2022-11-17 15:23:48 +0100

    Revert "using the list comprehenshion to obtain the output of the first 20 multiples of 3"
    This reverts commit 99e7d8671d5e0db8a6c536db29fb292957eba69c.

--- listcomp.py
+++ listcomp.py
@@ -6,10 +6,3 @@ names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smi
 
 first_names = [name.split()[0].lower() for name in names] # write your list comprehension here
 print(first_names)
-
-
-# Quiz: Multiples of Three
-# Use a list comprehension to create a list multiples_3 containing the first 20 multiples of 3.
-
-multiples_3 = [x*3 for x in range(1, 21)] # write your list comprehension here
-print(multiples_3)
