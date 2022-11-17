17b4a13416a7c9193e7b8fa610ded33a85c8b3d7
tuyojr <olutuyod@gmail.com>
2022-11-17 12:00:47 +0100

    using the zip method to create a dictionary of friends cast

--- zip_and_enumerate.py
+++ zip_and_enumerate.py
@@ -16,3 +16,17 @@ for point in zip(labels, x_coord, y_coord, z_coord):
 
 for point in points:
     print(point)
+
+
+
+
+
+
+# Quiz: Zip Lists to a Dictionary
+# Use zip to create a dictionary cast that uses names as keys and heights as values.
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights)) # replace with your code
+print(cast)
