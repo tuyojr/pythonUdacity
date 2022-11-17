2b0ca035bfadd621994e4fb36fbaea379375f6e1
tuyojr <olutuyod@gmail.com>
2022-11-17 06:26:09 +0100

    while loop to sum the first 5 odd numbers in a list

--- while_loops_II.py
+++ while_loops_II.py
@@ -91,3 +91,29 @@ print(nearest_square)
 # 1) a condition expression that will be assessed and when met, will allow you to exit the loop 
 # 2) make sure the loop is advancing 
 # 3) the value of the condition variables is changing with each iteration of the loop.
+
+
+
+
+# Your code should add up the odd numbers in the list, but only up to the first 
+# 5 odd numbers together. If there are more than 5 odd numbers, you should stop 
+# at the fifth. If there are fewer than 5 odd numbers, add all of the odd numbers.
+
+# Would you use a while or a for loop to write this code?
+
+num_list = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542, 
+            87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
+
+count_odd = 0
+list_sum = 0
+i = 0
+len_num_list = len(num_list)
+
+while (count_odd < 5) and (i < len_num_list): 
+    if num_list[i] % 2 != 0:
+        list_sum += num_list[i]
+        count_odd += 1
+    i += 1
+
+print ("The numbers of odd numbers added are: {}".format(count_odd))
+print ("The sum of the odd numbers added is: {}".format(list_sum))
