6701e4679baae2abf2dbd08e2b1ee59f9879cfde
tuyojr <olutuyod@gmail.com>
2022-11-18 10:00:39 +0100

    simple function to calculate the population density of an area

--- defining_functions.py
+++ defining_functions.py
@@ -6,3 +6,15 @@
 # your function works correctly. Once you've written your 
 # function, use the Test Run button to test your code.
 
+# write your function here
+def population_density(population, land_area):
+    return population / land_area
+
+# test cases for your function
+test1 = population_density(10, 1)
+expected_result1 = 10
+print("expected result: {}, actual result: {}".format(expected_result1, test1))
+
+test2 = population_density(864816, 121.4)
+expected_result2 = 7123.6902801
+print("expected result: {}, actual result: {}".format(expected_result2, test2))
