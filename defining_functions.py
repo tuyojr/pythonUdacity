ea1a7a440a5ad822a1b31e1771a7fe4f2b564957
tuyojr <olutuyod@gmail.com>
2022-11-18 09:58:40 +0100

    population density function task

---
+++ defining_functions.py
@@ -0,0 +1,8 @@
+# Quiz: Population Density Function
+# Write a function named population_density that takes 
+# two arguments, population and land_area, and returns a 
+# population density calculated from those values. I've 
+# included two test cases that you can use to verify that 
+# your function works correctly. Once you've written your 
+# function, use the Test Run button to test your code.
+
