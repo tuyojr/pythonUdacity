7792280fdc07fc78e614e11989592cbca159c5f8
tuyojr <olutuyod@gmail.com>
2022-11-18 10:36:23 +0100

    lambda with map, quiz

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+# Quiz: Lambda with Map
+# map() is a higher-order built-in function that takes a 
+# function and iterable as inputs, and returns an iterator 
+# that applies the function to each element of the iterable. 
+# The code below uses map() to find the mean of each list in 
+# numbers to create the list averages. Give it a test run to 
+# see what happens.
+
+# Rewrite this code to be more concise by replacing the mean 
+# function with a lambda expression defined within the call to map().
+
