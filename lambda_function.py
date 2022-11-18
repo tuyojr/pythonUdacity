65acddf1b311653096c57af0a4878b2e5797d875
tuyojr <olutuyod@gmail.com>
2022-11-18 10:45:09 +0100

    using the lambda function and the filter method to obtain cities with characters less than 10

--- lambda_function.py
+++ lambda_function.py
@@ -37,3 +37,11 @@ print(averages)
 
 # Rewrite this code to be more concise by replacing the is_short 
 # function with a lambda expression defined within the call to filter().
+
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+# def is_short(name):
+#     return len(name) < 10
+
+short_cities = list(filter(lambda name: len(name) < 10, cities))
+print(short_cities)
