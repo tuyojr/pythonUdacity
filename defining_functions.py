710e78046675e194405d3b23ab36f31db0054e27
tuyojr <olutuyod@gmail.com>
2022-11-18 10:09:23 +0100

    simple function to calculate the number of weeks and days

--- defining_functions.py
+++ defining_functions.py
@@ -18,3 +18,23 @@ print("expected result: {}, actual result: {}".format(expected_result1, test1))
 test2 = population_density(864816, 121.4)
 expected_result2 = 7123.6902801
 print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+
+
+
+# Quiz: readable_timedelta
+# Write a function named readable_timedelta. The function should 
+# take one argument, an integer days, and return a string that 
+# says how many weeks and days that is. For example, calling the 
+# function and printing the result like this:
+
+# write your function here
+def readable_timedelta(days):
+    # use integer division to get the number of weeks
+    weeks = days // 7
+    # use % to get the number of days that remain
+    remainder = days % 7
+    return "{} week(s) and {} day(s).".format(weeks, remainder)
+        
+# test your function
+print(readable_timedelta(10))
