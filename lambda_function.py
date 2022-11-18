d240c006898b4d4510b25ab043a0d3d4039cdef3
tuyojr <olutuyod@gmail.com>
2022-11-18 10:41:08 +0100

    using a lambda function with the map method

--- lambda_function.py
+++ lambda_function.py
@@ -9,3 +9,16 @@
 # Rewrite this code to be more concise by replacing the mean 
 # function with a lambda expression defined within the call to map().
 
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+
+# def mean(num_list):
+#     return sum(num_list) / len(num_list)
+
+averages = list(map(lambda num_list: sum(num_list) / len(num_list), numbers))
+print(averages)
