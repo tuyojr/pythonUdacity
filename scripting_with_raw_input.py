410fd0f88cae8f827d66008037991efac23badc1
tuyojr <olutuyod@gmail.com>
2022-11-19 17:16:27 +0100

    quiz: generate messages

--- scripting_with_raw_input.py
+++ scripting_with_raw_input.py
@@ -1,3 +1,32 @@
+# Quiz: Generate Messages
+# Imagine you're a teacher who needs to send a message to each 
+# of your students reminding them of their missing assignments 
+# and grade in the class. You have each of their names, number 
+# of missing assignments, and grades on a spreadsheet and just 
+# have to insert them into placeholders in this message you came 
+# up with:
+
+# Hi [insert student name],
+
+# This is a reminder that you have [insert number of missing assignments] 
+# assignments left to submit before you can graduate. Your current grade 
+# is [insert current grade] and can increase to [insert potential grade] 
+# if you submit all assignments before the due date.
+
+# You can just copy and paste this message to each student and manually 
+# insert the appropriate values each time, but instead you're going to 
+# write a program that does this for you.
+
+# Write a script that does the following:
+
+# Ask for user input 3 times. Once for a list of names, once for a list of 
+# missing assignment counts, and once for a list of grades. Use this input 
+# to create lists for names, assignments, and grades.
+# Use a loop to print the message for each student with the correct values. 
+# The potential grade is simply the current grade added to two times the 
+# number of missing assignments.
+
+
 names = input("Enter names separated by commas: ").title().split(",") # get and process input for a list of names
 assignments = input("Enter assignment counts separated by commas: ").split(",") # get and process input for a list of the number of assignments
 grades = input("Enter grades separated by commas: ").split(",") # get and process input for a list of grades
