949e9c7d13a2c1ee766d966b1d3e101b470b947c
tuyojr <olutuyod@gmail.com>
2022-11-20 14:35:12 +0100

    simple algorithm to create a file, write to it and display it's content

--- reading_and_writing_files.py
+++ reading_and_writing_files.py
@@ -3,3 +3,14 @@ file_data = f.read()
 f.close()
 
 print(file_data)
+
+
+
+writing = open('file', 'w')
+writing.write('This is just a sample file.')
+writing.close()
+writing = open('file')
+files = writing.read()
+writing.close()
+
+print(files)
