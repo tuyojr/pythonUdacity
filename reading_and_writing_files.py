4541bc12001147d31f4d7a13b8647e710107498d
tuyojr <olutuyod@gmail.com>
2022-11-20 14:47:08 +0100

    Quiz: Flying Circus Cast List

--- reading_and_writing_files.py
+++ reading_and_writing_files.py
@@ -14,3 +14,18 @@ files = writing.read()
 writing.close()
 
 print(files)
+
+
+
+
+# Quiz: Flying Circus Cast List
+# You're going to create a list of the actors who appeared in the 
+# television programme Monty Python's Flying Circus.
+
+# Write a function called create_cast_list that takes a filename as 
+# input and returns a list of actors' names. It will be run on the 
+# file flying_circus_cast.txt (this information was collected from imdb.com). 
+# Each line of that file consists of an actor's name, a comma, and 
+# then some (messy) information about roles they played in the programme. 
+# You'll need to extract only the name and add it to a list. You might 
+# use the .split() method to process each line.
