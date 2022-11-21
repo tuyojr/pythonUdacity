2a53d68acbbac748ee4779b86abf8384e7a3609f
tuyojr <olutuyod@gmail.com>
2022-11-21 12:23:04 +0100

    techniques for importing modules

---
+++ techniques_for_importing_modules.py
@@ -0,0 +1,17 @@
+# Importing and accessing from modules
+# In this quiz, you'll be using different methods to import 
+# and use the random.randint() function from the random module. 
+# Your task is to match the import statement with the way you 
+# would then call the function itself.
+
+# Match the import statement with the way that random.randint() is called.
+
+import random # ===> random.randint(0, 10)
+
+from random import randint # ===> randint(0, 10)
+
+import random as rd # ===> rd.randint(0, 10)
+
+from random import randint as rint # ===> rint(0, 10)
+
+from random import * # ===> Don't use this import statement
