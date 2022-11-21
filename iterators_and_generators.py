5e0f3d40303b51c5ad1abd3840fd14ffa00536f9
tuyojr <olutuyod@gmail.com>
2022-11-21 15:25:03 +0100

    creating a generator function that works like the built in enumerate function

---
+++ iterators_and_generators.py
@@ -0,0 +1,28 @@
+# Quiz: Implement my_enumerate
+# Write your own generator function that works like the built-in function enumerate.
+
+# Calling the function like this:
+
+# lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+# for i, lesson in my_enumerate(lessons, 1):
+#     print("Lesson {}: {}".format(i, lesson))
+# should output:
+
+# Lesson 1: Why Python Programming
+# Lesson 2: Data Types and Operators
+# Lesson 3: Control Flow
+# Lesson 4: Functions
+# Lesson 5: Scripting
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    # Implement your generator function here
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
