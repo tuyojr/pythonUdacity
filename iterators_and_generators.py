7f160fc5d15556c2ff24b626a5260e9fe2c9eb89
tuyojr <olutuyod@gmail.com>
2022-11-21 15:31:13 +0100

    a generator function that takes in an iterable and yields a chunk of a specified size at a time

--- iterators_and_generators.py
+++ iterators_and_generators.py
@@ -26,3 +26,37 @@ def my_enumerate(iterable, start=0):
 
 for i, lesson in my_enumerate(lessons, 1):
     print("Lesson {}: {}".format(i, lesson))
+
+
+
+
+
+# Quiz: Chunker
+# If you have an iterable that is too large to fit in memory in full 
+# (e.g., when dealing with large files), being able to take and use 
+# chunks of it at a time can be very valuable.
+
+# Implement a generator function, chunker, that takes in an iterable 
+# and yields a chunk of a specified size at a time.
+
+# Calling the function like this:
+
+# for chunk in chunker(range(25), 4):
+#     print(list(chunk))
+# should output:
+
+# [0, 1, 2, 3]
+# [4, 5, 6, 7]
+# [8, 9, 10, 11]
+# [12, 13, 14, 15]
+# [16, 17, 18, 19]
+# [20, 21, 22, 23]
+# [24]
+
+def chunker(iterable, size):
+    # Implement function here
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
