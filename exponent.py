fb69f2388e560e6f922b87aa75e92447f058d9af
tuyojr <olutuyod@gmail.com>
2022-11-21 09:47:36 +0100

    simple algorithm, using the math module to compute an exponent

---
+++ exponent.py
@@ -0,0 +1,12 @@
+# Use the math module to calculate e to the power of 3. print the answer.
+# Refer to https://docs.python.org/3/library/math.html to get the help you need.
+
+# print e to the power of 3 using the math module
+
+import math
+
+print(pow(math.e, 3))
+
+# OR
+
+print(math.exp(3))
