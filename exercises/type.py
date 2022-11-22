661ca3db066007502b91e9e14ac8b4006267b262
tuyojr <olutuyod@gmail.com>
2022-11-22 16:48:09 +0100

    simple program to convert from celcius to fahrenheit

--- exercises/type.py
+++ exercises/type.py
@@ -4,3 +4,12 @@ height = 12.0
 print(type(width//2))
 print(type(width/2.0))
 print(type(height/3))
+
+
+# Write a program which prompts the user for a Celsius temperature, 
+# convert the temperature to Fahrenheit, and print out the converted temperature.
+
+temp = float(input('Enter the temperature in Celcius: '))
+converter = (temp * 1.8) + 32
+
+print('{}°C converted to fahrenheit is {}°F'.format(temp, converter))
