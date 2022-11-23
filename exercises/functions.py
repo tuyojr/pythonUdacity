5be752f37308b03a07b0d029d3c3abe544271b1e
tuyojr <olutuyod@gmail.com>
2022-11-23 20:29:07 +0100

    simple function to calculate hourly pay increased by 1.5 time for hours above 40

--- exercises/functions.py
+++ exercises/functions.py
@@ -4,3 +4,15 @@ import random
 for i in range(10):
     x = random.random()
     print(x)
+    
+hours = float(input('Enter Hours: '))
+rate = float(input('Enter Rate: '))
+
+def pay_computation():
+    if hours <= 40:
+        pay = hours * rate
+    else:
+        pay = ((hours - 40) * (rate * 1.5) + (40 * rate))
+    return 'Your pay for this weeks is: {}'.format(pay)
+
+print(pay_computation())
