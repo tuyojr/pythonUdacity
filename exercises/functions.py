00661064bc9fd72ad55845703cd5466c854302e6
tuyojr <olutuyod@gmail.com>
2022-11-23 20:36:02 +0100

    simple function to display grades

--- exercises/functions.py
+++ exercises/functions.py
@@ -16,3 +16,23 @@ def pay_computation():
     return 'Your pay for this weeks is: {}'.format(pay)
 
 print(pay_computation())
+
+score = input('Please enter your score (between 0.0 and 1.0): ')
+
+def grader():
+    float(score)
+    if float(score) >= 0.9 and float(score) <= 1.0:
+        print('Perfect Score! Your grade is A.')
+    elif float(score) >= 0.8 and float(score) <= 0.9:
+        print('Great Score! Your grade is B.')
+    elif float(score) >= 0.7 and float(score) <= 0.8:
+        print('Good Score! Your grade is C.')
+    elif float(score) >= 0.6 and float(score) <= 0.7:
+        print('Well Done! Your grade is D.')
+    elif float(score) >= 0.0 and float(score) <= 0.6:
+        print('You can always do better, you got F.')
+    else:
+        print('Invalid Score!')
+
+grader()
+
