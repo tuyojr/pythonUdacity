be9a93b52fba3faf1e94134e235065a7ac204828
tuyojr <olutuyod@gmail.com>
2022-11-23 18:56:47 +0100

    simple algorithm to calculate hourly pay increased by 1.5 time for hours above 40

---
+++ exercises/conditional_I.py
@@ -0,0 +1,11 @@
+# Rewrite your pay computation to give the employee 1.5 times 
+# the hourly rate for hours worked above 40 hours.
+hours = float(input('Enter Hours:'))
+rate = float(input('Enter Rate:'))
+
+if hours <= 40:
+  pay = hours * rate
+else: 
+  pay = ((hours - 40) * (1.5 * rate)) + (40 * rate)
+
+print ('Pay ='), pay
