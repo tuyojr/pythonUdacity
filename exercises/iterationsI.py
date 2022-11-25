87ecf3add5d9280a6a8c39f1977e284b4c64a9bd
tuyojr <olutuyod@gmail.com>
2022-11-25 09:08:28 +0100

    simple algorithm to create a list of numbers from user input and print the largest and smallest num in the list

--- exercises/iterationsI.py
+++ exercises/iterationsI.py
@@ -24,3 +24,35 @@ while True:
 
 print('You entered a total of {} numbers, the sum total of those numbers equals {}, and the average of these numbers is {}.'.format(count, total, avg))
     
+
+
+
+
+# Exercise 2: Write another program that prompts for a list 
+# of numbers as above and at the end prints out both the maximum 
+# and minimum of the numbers instead of the average.
+
+user_list = []
+num_count = 0
+value = True
+
+# Use a while loop that executes a try block to update the user's list
+# Also we want to keep track of the numbers added to the list by updating the 
+# num_count variable
+while value:
+  try:
+    user_input = input('Enter a number you want to add to the list:\n>')
+    user_input = float(user_input)
+    user_list.append(user_input)
+    num_count = num_count + 1
+  except:
+    if user_input == 'Done' or user_input == 'done':
+      break
+    else:
+      print('Invalid user input.')
+      continue
+
+smallest_num = min(user_list)
+largest_num = max(user_list)
+
+print('The smallest number in your list is {}, and the largest number is {}.'.format(smallest_num, largest_num))
