721153e4350a5c25f3ba67240b73887fd2a23a0f
tuyojr <olutuyod@gmail.com>
2022-11-25 09:29:32 +0100

    simple while loop to print each letter in a string backwards

---
+++ exercises/loopI.py
@@ -0,0 +1,12 @@
+# Exercise 1: Write a while loop that starts at the last 
+# character in the string and works its way backwards to 
+# the first character in the string, printing each letter 
+# on a separate line, except backwards.
+
+value = 'Jermaine'
+index = 0
+
+while index < len(value):
+    letter = value[index]
+    print(letter)
+    index -= 1
