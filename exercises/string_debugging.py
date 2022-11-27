77abe537f4b5c5ef3f23888726f56ceff8bc6f96
tuyojr <olutuyod@gmail.com>
2022-11-27 09:02:17 +0100

    code to debug

---
+++ exercises/string_debugging.py
@@ -0,0 +1,11 @@
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
+
+# Code: http://www.py4e.com/code3/copytildone2.py
+# Or select Download from this trinket's left-hand menu
