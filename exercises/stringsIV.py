1627f9c669cb48b947ff0987ad8dc503a5de392f
tuyojr <olutuyod@gmail.com>
2022-11-27 09:24:31 +0100

    simple algorithm to extract a float from a string

--- exercises/stringsIV.py
+++ exercises/stringsIV.py
@@ -5,3 +5,11 @@
 # Use find and string slicing to extract the portion of the string 
 # after the colon character and then use the float function to convert 
 # the extracted string into a floating point number.
+
+str = 'X-DSPAM-Confidence:0.8475'
+
+colon = str.find(':')
+print("The colon is at index {}".format(colon))
+
+number = str[colon+1:]
+print('The floating point number is {}'.format(float(number)))
