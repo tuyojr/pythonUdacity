a87eee779fcaefe00e645f2e3175586da1346980
tuyojr <olutuyod@gmail.com>
2022-11-27 16:21:39 +0100

    print all the lines of a file that start with a particular word

---
+++ exercises/files.py
@@ -0,0 +1,11 @@
+# This prints a double space between each line
+f = open('demo.txt')
+# for line in f:
+#     if line.startswith('From:'):
+#         print(line)
+
+# This removes the double space
+for lines in f:
+    lines = lines.rstrip()
+    if lines.startswith('From:'):
+        print(lines)
