86d44b07e84923ae1836282bd980088d415a9e40
tuyojr <olutuyod@gmail.com>
2022-12-01 10:43:36 +0100

    a simple function to remove the first and last element from a user created list

---
+++ exercises/lists.py
@@ -0,0 +1,41 @@
+# Exercise 1:
+
+# Write a function called chop that takes a list and modifies it, 
+# removing the first and last elements, and returns None.
+
+# Then write a function called middle that takes a list and returns 
+# a new list that contains all but the first and last elements.
+
+def chop(a_list):
+    # delete the first element
+    del a_list[0]
+
+    # delete the last element
+    del a_list[-1]
+
+    return None
+
+print("Your inputs will be turned into a list. Type 'done' to exit and see your list.")
+
+# An empty list to hold all our users elements
+user_list = []
+
+# A while loop the continues to run if true
+while True:
+
+    # Prompt the user for input
+    elements = input("Please input what you want in your list: ")
+
+    # An if condition that breaks from the loop
+    if elements == 'done':
+        break
+
+    # Add the values the user input into the empty list
+    else:
+        user_list.append(elements)
+
+# call the chop function on the user's newly created list
+chop(user_list)
+
+# display the user's newly created list after chopping
+print(user_list)
