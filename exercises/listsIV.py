f74b026b69f31c0c5e6ce27055f2058abd828fa4
tuyojr <olutuyod@gmail.com>
2022-12-02 10:08:44 +0100

    simple function to sort a file alphabetically

--- exercises/listsIV.py
+++ exercises/listsIV.py
@@ -14,3 +14,29 @@
 # 'sun', 'the', 'through', 'what', 'window',
 # 'with', 'yonder']
 
+def file(user_file):
+    open_file = open(user_file)
+    new_list = list()
+    for line in open_file:
+        words = line.split()
+        for word in words:
+            if word not in new_list:
+                new_list.append(word)
+                new_list.sort()
+    return new_list
+
+
+print('You can input a file you want to open, with the correct extension.\nIf you wish to terminate the program without any interaction\njust type "done".\n')
+
+file_name = input('Enter a file name: ')
+count = 0
+
+while True:
+    if file_name == 'done':
+        print('Thank you for using this program.')
+        break
+    else:
+        count += 1
+        print(file(file_name))
+        exit()
+
