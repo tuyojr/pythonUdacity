2dd85de8c499c19f11b895058dc3a50fdf65e055
tuyojr <olutuyod@gmail.com>
2022-12-02 10:13:46 +0100

    simple function to sort a file alphabetically

--- exercises/listsIV.py
+++ exercises/listsIV.py
@@ -14,23 +14,41 @@
 # 'sun', 'the', 'through', 'what', 'window',
 # 'with', 'yonder']
 
+# Create a function that accepts a file name as user input
 def file(user_file):
+
+    # Open the file in read mode
     open_file = open(user_file)
+
+    # Create an empty list to store words from the file
     new_list = list()
+
+    # loop through each line in the file
     for line in open_file:
+
+        # split each line into words
         words = line.split()
+
+        # loop through each word in each line
         for word in words:
+
+            # if a word in the file is not in the empty list above, add it to it and sort the new_list
             if word not in new_list:
                 new_list.append(word)
                 new_list.sort()
+    # return the value of the new_list
     return new_list
 
-
+# Just a prompt to show the user what to do and how to use the program
 print('You can input a file you want to open, with the correct extension.\nIf you wish to terminate the program without any interaction\njust type "done".\n')
 
+# create a variable that stores the user input
 file_name = input('Enter a file name: ')
+
+# Not necessary, counts each time the loop runs
 count = 0
 
+# Simple while loop that runs if the user wants to continue or not with the program
 while True:
     if file_name == 'done':
         print('Thank you for using this program.')
