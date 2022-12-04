d42c4c11c3de162e92467d0bdc8175dbbe66865e
tuyojr <olutuyod@gmail.com>
2022-12-04 09:27:51 +0100

    exercise description

---
+++ exercises/dictI.py
@@ -0,0 +1,7 @@
+# Exercise 1: [wordlist2]
+
+# Write a program that reads the words in words.txt and stores 
+# them as keys in a dictionary. It doesn't matter what the values 
+# are. Then you can use the in operator as a fast way to check 
+# whether a string is in the dictionary.
+
