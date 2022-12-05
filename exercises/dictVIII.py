3857c22b6f89041065de9767cd49179a37c926cb
tuyojr <olutuyod@gmail.com>
2022-12-05 10:27:45 +0100

    extracting the domain from senders email into a dictionary

--- exercises/dictVIII.py
+++ exercises/dictVIII.py
@@ -8,4 +8,19 @@
 # {'media.berkeley.edu': 4, 'uct.ac.za': 6, 'umich.edu': 7,
 # 'gmail.com': 1, 'caret.cam.ac.uk': 1, 'iupui.edu': 8}
 
+file_name = input('Enter the file name with a correct extension: ')
+email_domain = {}
+file_list = []
 
+with open(file_name) as f:
+    for line in f:
+        file_list = line.split()
+        if line.startswith('From'):
+            mail = file_list[1]
+            print(mail)
+            domain = mail.split('@')[1]
+            if domain not in email_domain:
+                email_domain[domain] = 1
+            else:
+                email_domain[domain] += 1
+    print(email_domain)
