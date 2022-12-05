778bdfa818dda1d9600d7777217a5afac9827ce3
tuyojr <olutuyod@gmail.com>
2022-12-05 08:35:50 +0100

    extracting the days and number of times messages were sent, into a dictionary

--- exercises/dictV.py
+++ exercises/dictV.py
@@ -12,16 +12,33 @@
 # Enter a file name: mbox-short.txt
 # {'Fri': 20, 'Thu': 6, 'Sat': 1}
 
+# create an empty dictionary to hold the days of the week messages were sent
 days = {}
+
+# create an empty list to hold the recurring days
 days_list = []
+
+# Prompt the user to enter the file name
 file_name = input('Enter the file name with a correct extension: ')
 
+# use the with method to open the file and close it when done
 with open(file_name) as f:
+
+    # loop through each line of the file
     for line in f:
+
+        # set the value of the empty list above to each line in the file 
         days_list = line.split()
+
+        # check if the line in the list is more than 3 words and starts with the keyword we want.
+        # if it is, set the value of a new variable that stores the 3rd index which is where what
+        # we need is.
+        # next, check if the value is not in our dictionary and set it with a value of 1
+        # if the value is already there, we just increase the value, indicating how many times it occurs.
+        # lastly we print our new value to the screen
         if len(days_list) > 3 and line.startswith('From'):
             day = days_list[2]
-        if day not in days:
+        elif day not in days:
             days[day] = 1
         else:
             days[day] += 1
