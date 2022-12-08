fbe5f45480bf013a70e54da8d4eceaba559d823b
tuyojr <olutuyod@gmail.com>
2022-12-08 11:14:50 +0100

    exercise description

---
+++ exercises/tuplesIII.py
@@ -0,0 +1,8 @@
+# Write a program that reads a file and prints the letters 
+# in decreasing order of frequency. Your program should 
+# convert all the input to lower case and only count the 
+# letters a-z. Your program should not count spaces, digits, 
+# punctuation, or anything other than the letters a-z. Find 
+# text samples from several different languages and see how 
+# letter frequency varies between languages. Compare your 
+# results with the tables at https://wikipedia.org/wiki/Letter_frequencies.
