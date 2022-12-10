fe1ab76ebd62b2b38356923676f080a8b1c207a2
tuyojr <olutuyod@gmail.com>
2022-12-10 11:09:28 +0100

    exercise description

---
+++ exercises/regexII.py
@@ -0,0 +1,12 @@
+# Write a program to look for lines of the form
+
+# `New Revision: 39772`
+# and extract the number from each of the lines using a regular 
+# expression and the findall() method. Compute the average of 
+# the numbers and print out the average.
+
+# Enter file:mbox.txt
+# 38549.7949721
+
+# Enter file:mbox-short.txt
+# 39756.9259259
