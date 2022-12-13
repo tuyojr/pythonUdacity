0278f003ec3a244d64410aebd35cfa0acfc4e3b1
tuyojr <olutuyod@gmail.com>
2022-12-13 11:33:39 +0100

    adding a .txt extension to the created file

--- offline/date_time.py
+++ offline/date_time.py
@@ -15,7 +15,7 @@ def create_file():
     # be named using the strftime method, because the datetime.datetime.now() format
     # will not be accepted as a correct name for the file
     # %Y = Year, %m = month, %d = day, %H = Hour, %M = Minutes
-    with open(file_name.strftime("%Y-%m-%d-%H-%M"), "w") as file:
+    with open(file_name.strftime("%Y-%m-%d-%H-%M") + ".txt", "w") as file:
         file.write("This file was created on {} at {}min(s) past {}hour(s).".format(file_name.strftime('%d-%m-%Y'),file_name.strftime('%M'),file_name.strftime('%H')))
 
 create_file()
