9714059acb4d3edf6236ae667f1cf3f53d7dad60
tuyojr <olutuyod@gmail.com>
2022-12-15 12:41:49 +0100

    inheritance

---
+++ exercises/classV.py
@@ -0,0 +1,18 @@
+from classIV import PartyAnimal
+
+class Football(PartyAnimal):
+   points = 0
+   def six(self):
+      self.points = self.points + 3
+      self.party()
+      print(self.name,"points",self.points)
+
+s = PartyAnimal("Sally")
+s.party()
+j = Football("Jim")
+j.party()
+j.six()
+print(dir(j))
+
+# Code: http://www.py4e.com/code3/party6.py
+# Or select Download from this trinket's left-hand menu
